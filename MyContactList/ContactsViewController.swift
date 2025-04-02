a92216b03628b7332bb2ec56a3a490d6aa47ad51
Anh Phan <anhnguyenphan03@gmail.com>
2025-04-02 19:05:40 -0400

    Chapter 10 Done

--- MyContactList/ContactsViewController.swift
+++ MyContactList/ContactsViewController.swift
@@ -8,15 +8,60 @@
 import UIKit
 
 class ContactsViewController: UIViewController {
-
+    @IBOutlet weak var sgmtEditMode: UISegmentedControl!
+    
+    @IBOutlet weak var txtName: UITextField!
+    
+    @IBOutlet weak var txtAddress: UITextField!
+    
+    @IBOutlet weak var txtCity: UITextField!
+    
+    @IBOutlet weak var txtState: UITextField!
+    
+    
+    @IBOutlet weak var txtZipcode: UITextField!
+    
+    @IBOutlet weak var txtCell: UITextField!
+    
+    @IBOutlet weak var txtPhone: UITextField!
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    @IBOutlet weak var lblBirthdate: UILabel!
+    
+    @IBOutlet weak var btnChange: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.changeEditMode(self)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
     @IBOutlet weak var scrollView: UIScrollView!
     
+    @IBAction func changeEditMode(_ sender: Any) {
+        let textFields: [UITextField] = [txtName, txtAddress, txtCity, txtState, txtZipcode, txtPhone, txtCell, txtEmail]
+        if sgmtEditMode.selectedSegmentIndex == 0 {
+            for textField in textFields {
+                textField.isEnabled = false
+                textField.borderStyle = UITextField.BorderStyle.none
+            }
+            btnChange.isHidden = true
+        }
+        else if sgmtEditMode.selectedSegmentIndex == 1 {
+            for textField in textFields {
+                textField.isEnabled = true
+                textField.borderStyle = UITextField.BorderStyle.roundedRect
+            }
+            btnChange.isHidden = false
+        }
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.registerKeyboardNotifications()
