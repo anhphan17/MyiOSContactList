20b4869a3c201cf7e1351d55690bbe53cf7a5043
Anh Phan <anhnguyenphan03@gmail.com>
2025-04-15 18:22:02 -0400

    Creating the Settings Interface

--- MyContactList/SettingsViewController.swift
+++ MyContactList/SettingsViewController.swift
@@ -7,12 +7,21 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var pckSortField: UIPickerView!
+    
+    @IBOutlet weak var swAscending: UISwitch!
+    
+    let sortOrderItems: Array<String>
+ = ["ContactName", "City", "Birthday"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        pckSortField.dataSource = self;
+        pckSortField.delegate = self;
     }
     
 
@@ -25,5 +34,24 @@ class SettingsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    
+    @IBAction func sortDirectionChanged(_ sender: Any) {
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sortOrderItems.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sortOrderItems[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Chosen item: \(sortOrderItems[row])")
+    }
 }
