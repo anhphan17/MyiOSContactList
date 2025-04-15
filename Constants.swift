7980b445b379d1acf3e73871fb55110e9445dac4
Anh Phan <anhnguyenphan03@gmail.com>
2025-04-15 19:13:09 -0400

    Global Constants

---
+++ Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MyContactList
+//
+//  Created by Anh Phan on 4/15/25.
+//
+
+import Foundation
+struct Constants {
+    static let kSortField = "sortField"
+    static let kSortDirectionAscending = "sortDirectionAscending"
+}
--- MyContactList/AppDelegate.swift
+++ MyContactList/AppDelegate.swift
@@ -11,21 +11,21 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let settings = UserDefaults.standard
         
-        if settings.string(forKey: "sortField") == nil {
-            settings.set("City", forKey: "sortField")
+        if settings.string(forKey: Constants.kSortField) == nil {
+            settings.set("City", forKey: Constants.kSortField)
         }
         
-        if settings.string(forKey: "sortDirectionAscending") == nil {
-            settings.set(true, forKey: "sortDirectionAscending")
+        if settings.string(forKey: Constants.kSortDirectionAscending) == nil {
+            settings.set(true, forKey: Constants.kSortDirectionAscending)
         }
         settings.synchronize()
-        print("Sort field: \(settings.string(forKey: "sortField")!)")
-        print("Sort direction: \(settings.bool(forKey: "sortDirectionAscending"))")
+        NSLog("Sort field: %@", settings.string(forKey: Constants.kSortField)!)
+        NSLog("Sort direction: \(settings.bool(forKey: Constants.kSortDirectionAscending))")
+        
         return true
     }
 
