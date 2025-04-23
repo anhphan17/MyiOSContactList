c3b550e95711478da86bc5d0b5dcb0b8aa148653
Anh Phan <anhnguyenphan03@gmail.com>
2025-04-23 15:59:21 -0400

    Finding the Device Location (ch. 13)

--- MyContactList/LocationDemoViewController.swift
+++ MyContactList/LocationDemoViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import CoreLocation
 
-class LocationDemoViewController: UIViewController {
+class LocationDemoViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var txtStreet: UITextField!
     
@@ -32,6 +32,8 @@ class LocationDemoViewController: UIViewController {
     
     lazy var geoCoder = CLGeocoder()
     
+    var locationManager: CLLocationManager!
+    
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
@@ -41,6 +43,66 @@ class LocationDemoViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+        
+        //Set up location manager
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization() //ask for permission to use location
+    }
+    
+    //Called when the location permission status changes
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            print("Permission granted")
+        }
+        else {
+            print("Permission NOT granted")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let eventDate = location.timestamp
+            let howRecent = eventDate.timeIntervalSinceNow
+            if Double(howRecent) < 15.0 {
+                let coordinate = location.coordinate
+                lblLongitude.text = String(format: "%g\u{00B0}", coordinate.longitude)
+                lblLatitude.text = String(format: "%g\u{00B0}", coordinate.latitude)
+                lblLocationAccuracy.text = String(format: "%gm", location.horizontalAccuracy)
+                lblAltitude.text = String(format: "%gm", location.altitude)
+                lblAltitudeAccuracy.text = String(format: "%gm", location.verticalAccuracy)
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        if newHeading.headingAccuracy > 0 {
+            let theHeading = newHeading.trueHeading
+            var direction: String
+            switch theHeading {
+            case 225..<315:
+                direction = "W"
+            case 135..<225:
+                direction = "S"
+            case 45..<135:
+                direction = "E"
+            default:
+                direction = "N"
+            }
+            lblHeading.text = String(format: "%g\u{00B0} (%@)", theHeading, direction)
+            lblHeadingAccuracy.text = String(format: "%g\u{00B0}", newHeading.headingAccuracy)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        let errorType = error._code == CLError.denied.rawValue ? "Location Permission Denied" :
+        "Unknown Error"
+        let alertController = UIAlertController(title: "Error Getting Location: \(errorType)", message: "Error message: \(error.localizedDescription)", preferredStyle: .alert)
+        let actionOK = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(actionOK)
+        present(alertController, animated: true, completion:  nil)
     }
     
     @IBAction func addressToCoordinates(_ sender: Any) {
@@ -69,6 +131,15 @@ class LocationDemoViewController: UIViewController {
     }
     
     @IBAction func deviceCoordinates(_ sender: Any) {
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 100
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        locationManager.stopUpdatingLocation()
+        locationManager.stopUpdatingHeading()
     }
     /*
     // MARK: - Navigation
